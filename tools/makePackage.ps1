71c72c850df1426f42c454d3638fcf88240f9187
S-Katagiri <katagiri.stsh@gmail.com>
2023-01-19 23:15:01 +0900

    to packing corrected text for preinstalled modules

--- tools/makePackage.ps1
+++ tools/makePackage.ps1
@@ -1,10 +1,25 @@
-Remove-Item -Recurse Packages/CorrectLocalizationJP-LateMin  # power shell is very inconvenient
-Remove-Item -Recurse Packages/CorrectLocalizationJP-GenShin  # power shell is very inconvenient
+Remove-Item -Force -Recurse Packages/CorrectLocalizationJP-LateMin  # power shell is very inconvenient
+Remove-Item -Force -Recurse Packages/CorrectLocalizationJP-GenShin  # power shell is very inconvenient
 mkdir -Force Packages/CorrectLocalizationJP-LateMin
 mkdir -Force Packages/CorrectLocalizationJP-GenShin
-Copy-Item -Recurse -Force Modules/Common/* Packages/CorrectLocalizationJP-LateMin
-Copy-Item -Recurse -Force Modules/Common/* Packages/CorrectLocalizationJP-GenShin
-Copy-Item -Recurse -Force Modules/Font-GenShinGothic/* Packages/CorrectLocalizationJP-GenShin
-Copy-Item -Recurse -Force Modules/Font-LateMin/* Packages/CorrectLocalizationJP-LateMin
+Copy-Item -Recurse -Force Modules/CLJP-Common/* Packages/CorrectLocalizationJP-LateMin
+Copy-Item -Recurse -Force Modules/CLJP-Common/* Packages/CorrectLocalizationJP-GenShin
+Copy-Item -Recurse -Force Modules/CLJP-Font-GenShinGothic/* Packages/CorrectLocalizationJP-GenShin
+Copy-Item -Recurse -Force Modules/CLJP-Font-LateMin/* Packages/CorrectLocalizationJP-LateMin
 Compress-Archive -Force Packages/CorrectLocalizationJP-GenShin Packages/CorrectLocalizationJP-GenShin.zip
 Compress-Archive -Force Packages/CorrectLocalizationJP-LateMin Packages/CorrectLocalizationJP-LateMin.zip
+Get-ChildItem Modules | ForEach-Object {
+    if ( -not ($_.Name.Substring(0, 5) -eq 'CLJP-')){
+        Write-Output $_.Name
+        Compress-Archive -Force Modules/$($_.Name)/ Packages/$($_.Name)-JP.zip
+    }
+}
+Remove-Item -Force -Recurse Packages/CorrectText-JP
+mkdir -Force Packages/CorrectText-JP
+Get-ChildItem Modules | ForEach-Object {
+    if ( -not ($_.Name.Substring(0, 5) -eq 'CLJP-')){
+        Write-Output $_.Name
+        Copy-Item -Force -Recurse Modules/$($_.Name) Packages/CorrectText-JP/
+    }
+}
+Compress-Archive -Force Packages/CorrectText-JP Packages/CorrectText-JP.zip
