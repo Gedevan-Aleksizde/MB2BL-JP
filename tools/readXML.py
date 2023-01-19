878bf81a19860550a28194806f5b72f083382d0b
S-Katagiri <katagiri.stsh@gmail.com>
2023-01-19 23:14:03 +0900

    to update older text

--- tools/readXML.py
+++ tools/readXML.py
@@ -52,17 +52,32 @@ d['EN'] = pd.concat(d['EN'])
 d['EN'] = d['EN'].assign(text_EN = lambda d: d['text_EN'].str.replace('[\u00a0\u180e\u2007\u200b\u200f\u202f\u2060\ufeff]', '', regex=True))
 for lang in langs:
     d[lang] = pd.concat(d[lang])
-    d[lang][f'text_{lang}'] = d[lang][f'text_{lang}'].str.replace('[\u00a0\u180e\u2007\u200b\u200f\u202f\u2060\ufeff]', '', regex=True)
-# そんなに制御文字入れる必要ある?
-# なぜか重複しているIDが大量にある. ID なのに. 念のためファイル名でも対応させる
+    d[lang][f'text_{lang}_original'] = d[lang][f'text_{lang}_original'].str.replace('[\u00a0\u180e\u2007\u200b\u200f\u202f\u2060\ufeff]', '', regex=True)
+# TODO: do you really want to use control letters?
 d['JP']['file'] = d['JP']['file'].str.replace(r'^(.+)-jpn\.xml', r'\1.xml', regex=True)
 d_bilingual = d['EN'].merge(d['JP'], on=['id', 'file', 'module'], how='left')
-d_bilingual[
-    ['id', 'text_EN'] + [x for x in d_bilingual.columns if (x[:5] == 'text_' and x[-2:] != 'EN')] + ['file', 'module']
-].sort_values(['id', 'file']).to_excel('csv/languages.xlsx', index=False)
+d_bilingual = d_bilingual[
+    ['module', 'file', 'id', 'text_EN'] + [x for x in d_bilingual.columns if (x[:5] == 'text_' and x[-2:] != 'EN')]
+].sort_values(['id', 'file'])
+print(f'new text has {d_bilingual.shape[0]} entries')
 
-a = pd.read_excel("languages.ods", engine='odf')
-
-d_bilingual[
-    ['id', 'text_EN'] + [x for x in d_bilingual.columns if (x[:5] == 'text_' and x[-2:] != 'EN')] + ['file', 'module']
-].sort_values(['id', 'file']).merge(a.assign(module='Native'), on=['text_EN', 'id', 'file', 'module'], how='left').to_excel('aaaa.xlsx', index=False)
+with Path('text/languages-old.xlsx') as fp:
+    if fp.exists():
+        d_old = pd.read_excel(fp)
+        d_old = d_old[['module', 'id', 'file'] + [x for x in d_old if x[:5] == 'text_']]
+        n = d_bilingual.merge(d_old, on=['module', 'file', 'id'], how='inner').shape[0]
+        d_bilingual =  d_bilingual.merge(
+            d_old.rename(columns={
+                f'text_EN': 'text_EN_old',
+                f'text_{lang}_original': f'text_{lang}_original_old'}),
+            on=['module', 'file', 'id'],
+            how='left'
+            )
+        print(f'{d_bilingual.shape[0] - n} entries are not matched with the old data')
+        d_bilingual[
+            ['module', 'file', 'id', 'text_EN_old', f'text_{lang}_original_old', f'text_{lang}', 'text_EN', f'text_{lang}_original']
+            ].assign(
+                updated_en=lambda d: d['text_EN'] != d['text_EN_old'],
+                updated_jp=lambda d: d['text_JP_original'] != d['text_JP_original_old']
+                ).to_excel('languages.xlsx', index=False)
+d_bilingual.to_excel('languages.xlsx', index=False)
