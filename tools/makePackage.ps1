b6ff002cbd124c1f74b14a10c61f46437fee4e46
S-Katagiri <katagiri.stsh@gmail.com>
2023-06-10 22:30:41 +0900

    new package

--- tools/makePackage.ps1
+++ tools/makePackage.ps1
@@ -1,10 +1,13 @@
 Remove-Item -Force -Recurse Packages/CorrectLocalizationJP-LateMin  # power shell is very inconvenient
 Remove-Item -Force -Recurse Packages/CorrectLocalizationJP-GenShin  # power shell is very inconvenient
+Remove-Item -Force -Recurse Packages/CorrectLocalizationJP-MgenPlus  # power shell is very inconvenient
 Remove-Item -Force -Recurse Packages/CorrectLocalizationJP-Text  # power shell is very inconvenient
 mkdir -Force Packages/CorrectLocalizationJP-LateMin/CorrectLocalizationJPFont-LateMin
 mkdir -Force Packages/CorrectLocalizationJP-LateMin/CorrectLocalizationJP-Text
 mkdir -Force Packages/CorrectLocalizationJP-GenShin/CorrectLocalizationJPFont-Genshin
 mkdir -Force Packages/CorrectLocalizationJP-LateMin/CorrectLocalizationJP-Text
+mkdir -Force Packages/CorrectLocalizationJP-MgenPlus/CorrectLocalizationJPFont-MgenPlus
+mkdir -Force Packages/CorrectLocalizationJP-MgenPlus/CorrectLocalizationJP-Text
 mkdir -Force Packages/CorrectLocalizationJP-Text
 #Copy-Item -Recurse -Force Modules/CLJP-Common/* Packages/CorrectLocalizationJP-LateMin
 #Copy-Item -Recurse -Force Modules/CLJP-Common/* Packages/CorrectLocalizationJP-GenShin
@@ -15,15 +18,22 @@ Copy-Item -Recurse -Force Modules/CLJP-Font-GenShinGothic/* Packages/CorrectLoca
 Copy-Item -Recurse -Force Modules/CLJP-Common/* Packages/CorrectLocalizationJP-GenShin/CorrectLocalizationJP-Text
 Copy-Item -Force Modules/CLJP-Common/README.md Packages/CorrectLocalizationJP-GenShin/CorrectLocalizationJPFont-Genshin/
 Copy-Item -Force Modules/CLJP-Common/LICENSE Packages/CorrectLocalizationJP-GenShin/CorrectLocalizationJPFont-Genshin/
+
 Copy-Item -Recurse -Force Modules/CLJP-Font-LateMin/* Packages/CorrectLocalizationJP-LateMin/CorrectLocalizationJPFont-LateMin
 Copy-Item -Recurse -Force Modules/CLJP-Common/* Packages/CorrectLocalizationJP-LateMin/CorrectLocalizationJP-Text
 Copy-Item -Force Modules/CLJP-Common/README.md Packages/CorrectLocalizationJP-LateMin/CorrectLocalizationJPFont-LateMin/
 Copy-Item -Force Modules/CLJP-Common/LICENSE Packages/CorrectLocalizationJP-LateMin/CorrectLocalizationJPFont-LateMin/
 
+Copy-Item -Recurse -Force Modules/CLJP-Font-MgenPlus/* Packages/CorrectLocalizationJP-MgenPlus/CorrectLocalizationJPFont-MgenPlus
+Copy-Item -Recurse -Force Modules/CLJP-Common/* Packages/CorrectLocalizationJP-MgenPlus/CorrectLocalizationJP-Text
+Copy-Item -Force Modules/CLJP-Common/README.md Packages/CorrectLocalizationJP-MgenPlus/CorrectLocalizationJPFont-MgenPlus/
+Copy-Item -Force Modules/CLJP-Common/LICENSE Packages/CorrectLocalizationJP-MgenPlus/CorrectLocalizationJPFont-MgenPlus/
+
 Copy-Item -Recurse -Force Modules/CLJP-Common/* Packages/CorrectLocalizationJP-Text
 
 Compress-Archive -Force Packages/CorrectLocalizationJP-GenShin Packages/CorrectLocalizationJP-GenShin.zip
 Compress-Archive -Force Packages/CorrectLocalizationJP-LateMin Packages/CorrectLocalizationJP-LateMin.zip
+Compress-Archive -Force Packages/CorrectLocalizationJP-MgenPlus Packages/CorrectLocalizationJP-MgenPlus.zip
 Compress-Archive -Force Packages/CorrectLocalizationJP-Text Packages/CorrectLocalizationJP-Text.zip
 #Get-ChildItem Modules | ForEach-Object {
 #    if ( -not ($_.Name.Substring(0, 5) -eq 'CLJP-')){
