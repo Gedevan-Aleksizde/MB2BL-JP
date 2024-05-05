f27eeb3e8fa86f1d007a7933aeede61c1f33178c
S-Katagiri <katagiri.stsh@gmail.com>
2024-05-05 18:30:26 +0900

    why VSCode always fail to commit???

--- tools/export_vanilla_XML.py
+++ tools/export_vanilla_XML.py
@@ -4,15 +4,15 @@ import argparse
 import yaml
 from pathlib import Path
 import warnings
+from typing import Optional
 
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
+import lxml.etree as ET
+import html
 import pandas as pd
-from babel import Locale # Babel
-from babel.messages.pofile import read_po, write_po
-from babel.messages.mofile import read_mo, write_mo
-from babel.messages.catalog import Catalog
+import polib
 import regex
-from functions import po2pddf, removeannoyingchars, public_po, get_catalog_which_has_corrected_babel_fake_id, merge_yml
+from functions import po2pddf, removeannoyingchars, public_po, merge_yml
 
 pofile = Path('text/MB2BL-Jp.po')
 output = Path('Modules')
@@ -88,23 +88,19 @@ def export_modules(args, type):
     """
     print(f'output type: {type}')
     if args.input.exists():
-        with args.input.open('br') as f:
-            if args.input.suffix == '.po':
-                print(f'reading {args.input}')
-                catalog = read_po(f)
-            elif args.input.suffix == '.mo':
-                print(f'reading {args.input}')
-                catalog = read_mo(f)
-            else:
+        if args.input.suffix == '.po':
+            print(f'reading {args.input}')
+            pof = polib.pofile(args.input)
+        elif args.input.suffix == '.mo':
+            print(f'reading {args.input}')
+            pof = polib.pofile(args.input)
+        else:
                 raise('input file is invalid', UserWarning)
-    catalog = get_catalog_which_has_corrected_babel_fake_id(catalog)
-    catalog_pub = public_po(catalog)
-    with args.input.parent.joinpath(args.input.with_suffix('').name + '-pub.po').open('bw') as f:
-        write_po(f, catalog_pub)
-    with args.input.parent.joinpath(args.input.with_suffix('').name + '-pub.mo').open('bw') as f:
-        write_mo(f, catalog_pub)
-    del catalog_pub
-    d = po2pddf(catalog, drop_prefix_id=False)
+    pof_pub = public_po(pof)
+    pof_pub.save(args.input.parent.joinpath(args.input.with_suffix('').name + '-pub.po'))
+    pof_pub.save_as_mofile(args.input.parent.joinpath(args.input.with_suffix('').name + '-pub.mo'))
+    del pof_pub
+    d = po2pddf(pof, drop_prefix_id=False)
     if not args.legacy_id:
         d = pd.concat(
             [
@@ -117,7 +113,7 @@ def export_modules(args, type):
         d['duplication'] = d['duplication'].fillna(1)
     if args.skip_blank_vanilla:
         d = d.loc[lambda d: d['text'] != '']
-    del catalog
+    del pof
 
     if args.distinct:
         n = d.shape[0]
@@ -153,8 +149,7 @@ def export_modules(args, type):
             for xml_path in xml_list:
                 print(f'''Reading {xml_path.name} from {xml_path.parent.parent.parent.parent.name} Module''')
                 # edit language_data.xml
-                with xml_path.open('r', encoding='utf-8') as f:
-                    xml = BeautifulSoup(f, features='lxml-xml')
+                xml = ET.parse(xml_path)
                 en_xml_name = pd.Series(xml_path.with_suffix('').name).str.replace(f'''{args.filename_sep}{args.langsuffix}''', '')[0] + '.xml'
                 #TODO: refactoring
                 if args.legacy_id:
@@ -172,39 +167,38 @@ def export_modules(args, type):
                             warnings.warn(f'no match entries with {en_xml_name}! subsettings skipped, which cause a bit low performance.')
                     # TODO: language files get messed since v1.2.
                     # ファイルごとに分けることが無意味になった. IDさえ一意ならいいので元のファイルの分け方を守る必要もなさそう
-                if xml.find('base', recursive=False) is not None:
-                    default_lang_tag = xml.base.find('tag').get('language')
-                    if default_lang_tag != args.langid:
-                        xml.base.find('tags', recursive=False).append(generate_tag(args.langid))
+                if xml.getroot().tag == 'base':
+                    if xml.find('tags/tag').attrib['language'] != args.langid:
+                        xml.xpath('tags').append(generate_tag_element(args.langid))
                     if args.langalias is not None:
-                        xml.base.find('tags', recursive=False).append(generate_tag(args.langalias))
-                    if xml.base.find('strings', recursive=False) is not None:
-                        for string in xml.base.find('strings', recursive=False).find_all('string', recursive=False):
+                        xml.xpath('tags').append(generate_tag_element(args.langalias))
+                    if xml.find('strings') is not None:
+                        for string in xml.xpath('strings/string'):
                             n_entries_xml += 1
-                            tmp = d_sub.loc[lambda d: d['id'] == string['id']]
+                            tmp = d_sub.loc[lambda d: d['id'] == string.attrib['id']]
                             if tmp.shape[0] > 0 and tmp['text'].values[0] != '':
                                 new_str = removeannoyingchars(tmp['text'].values[0])
-                                if string['text'] != new_str or args.all_entries:
-                                    string['text'] = new_str
+                                if string.attrib['text'] != new_str or args.all_entries:
+                                    string.attrib['text'] = new_str
                                     n_change_xml += 1
                                 if not args.missing_modulewise:
-                                    d = d.loc[lambda d: d['id'] != string['id']]
+                                    d = d.loc[lambda d: d['id'] != string.attrib['id']]
                             else:
                                 if args.legacy_id:
-                                    warnings.warn(f'''ID not found: {string["id"]} in {module}/{xml_path.name}''')
-                                elif not d.loc[lambda d: d['id'] == string['id']].shape[0] > 0 and args.verbose:
-                                    warnings.warn(f'''ID not found: {string["id"]} in {module}/{xml_path.name}''')
-                                normalized_str = removeannoyingchars(string['text'])
-                                if normalized_str != string['text']:
+                                    warnings.warn(f'''ID not found: {string.attrib["id"]} in {module}/{xml_path.name}''')
+                                elif not d.loc[lambda d: d['id'] == string.attrib['id']].shape[0] > 0 and args.verbose:
+                                    warnings.warn(f'''ID not found: {string.attrib["id"]} in {module}/{xml_path.name}''')
+                                normalized_str = removeannoyingchars(string.attrib['text'])
+                                if normalized_str != string.attrib['text']:
                                     warnings.warn(
-                                        f'''this text could contain irregular characters (some control characters or zenkaku blanks): {string['text']}''',
+                                        f'''this text could contain irregular characters (some control characters or zenkaku blanks): {string.attrib['text']}''',
                                         UserWarning)
                                     n_change_xml += 1
-                                    string['text'] = normalized_str
+                                    string.attrib['text'] = normalized_str
                                 if args.distinct:
-                                    string.extract()
+                                    html.unescape((ET.tostring(string, encoding='unicode')))
                             if args.with_id:
-                                string['text'] = f"""[{string['id']}]{string['text']}"""
+                                string.attrib['text'] = f"""[{string.attrib['id']}]{string.attrib['text']}"""
                         if n_entries_xml > 0:
                             print(
                                 f'''{n_change_xml}/{n_entries_xml} ({100 * n_change_xml/n_entries_xml:.0f} %) text entries are changed in {xml_path.name}'''
@@ -215,13 +209,13 @@ def export_modules(args, type):
                         n_change_total += n_change_xml
                     else:
                         warnings.warn(f'{xml_path} is has no strings tag! processing skipped')
-                    language_data.LanguageData.append(
-                        generate_languageFile(f"{Path('/'.join([args.langfolder_output, module if type == 'module' else '', xml_path.name])).as_posix()}")
+                    language_data.getroot().append(
+                        generate_languageFile_element(f"{Path('/'.join([args.langfolder_output, module if type == 'module' else '', xml_path.name])).as_posix()}")
                         )
-                    output_dir.joinpath(f'''{xml_path.name}''').open('w', encoding='utf-8').writelines(xml.prettify(formatter='minimal'))
+                    write_xml_with_default_setting(xml, xml_path)
                 else:
                         warnings.warn(f'{xml_path} has no base tag! processing skipped')
-            output_dir.joinpath('language_data.xml').open('w', encoding='utf-8').writelines(language_data.prettify(formatter='minimal'))
+            write_xml_with_default_setting(language_data, output_dir.joinpath('language_data.xml'))
             if not args.suppress_missing_id and args.missing_modulewise:
                 print(f'------ Checking missing IDs in {module} ---------')
                 df_original = pd.read_excel('text/MB2BL-JP.xlsx')
@@ -234,27 +228,24 @@ def export_modules(args, type):
             print(f'''No language files found inside {base_langauge_path}''')
     if type=='module' and not args.no_english_overwriting:
         lang_data_patch = generate_language_data_xml(module='', id='English')
-        lang_data_patch.LanguageData.append(generate_languageFile(f'{args.langfolder_output}/Native/std_global_strings_xml_{args.langsuffix}.xml'))
-        with output_dir.joinpath('../../language_data.xml').open('w', encoding='utf-8') as f:
-            f.writelines(lang_data_patch.prettify())
+        lang_data_patch.getroot().append(generate_languageFile_element(f'{args.langfolder_output}/Native/std_global_strings_xml_{args.langsuffix}.xml'))
+        write_xml_with_default_setting(lang_data_patch, output_dir.joinpath('../../language_data.xml'))
     if type == 'module' and args.langalias is not None:
-        with args.output.joinpath(f'CL{args.langshort}-Common/ModuleData/Languages/{args.langfolder_output}/Native/language_data.xml').open('r', encoding='utf-8') as f:
-            language_data_alias = BeautifulSoup(f, features='lxml-xml')
+        with args.output.joinpath(f'CL{args.langshort}-Common/ModuleData/Languages/{args.langfolder_output}/Native/language_data.xml') as fp:
+            language_data_alias = ET.parse(fp)
         language_data = language_data_alias.find('LanguageData', recursive=False)
-        language_data['id'] = args.langalias
-        language_data['name'] = args.langalias
+        language_data.attrib['id'] = args.langalias
+        language_data.attrib['name'] = args.langalias
         xml_list = args.output.joinpath(f'CL{args.langshort}-Common/ModuleData/Languages/{args.langfolder_output}').rglob('language_data.xml')
         for fp in xml_list:
             if fp.parent != 'Native':
-                with fp.open('r', encoding='utf-8') as f:
-                    langauage_data2 =  BeautifulSoup(f, features='lxml-xml')
-                    for xml_languagefile in langauage_data2.find_all('LanguageFile'):
-                        language_data_alias.find('LanguageData').append(xml_languagefile)
+                langauage_data2 =  ET.parse(f)
+                for xml_languagefile in langauage_data2.findall('LanguageFile'):
+                    language_data_alias.getroot().append(xml_languagefile)
         output_fp = args.output.joinpath(f'CL{args.langshort}-Common/ModuleData/Languages/{args.langfolder_output}2/language_data.xml')
         if not output_fp.parent.exists():
             output_fp.parent.mkdir(parents=True)
-        with output_fp.open('w', encoding='utf-8') as f:
-            f.writelines(language_data_alias.prettify())
+        write_xml_with_default_setting(language_data_alias, output_fp)
     if n_entries_total > 0:
         print(f'''SUMMARY: {n_change_total}/{n_entries_total} ({100 * n_change_total/n_entries_total:.0f}%) text entries are changed totally''')
     if not args.suppress_missing_id and not args.missing_modulewise and d.shape[0] > 0:
@@ -263,15 +254,15 @@ def export_modules(args, type):
         if not output_dir.exists():
             output_dir.mkdir()
         language_data_missings = generate_language_data_xml(module='', id=args.langid)
-        language_data_missings.LanguageData.append(generate_languageFile(f'{args.langfolder_output}/Missings/str_missings-{args.langsuffix}.xml'))
-        language_data_missings.LanguageData.append(generate_languageFile(f'{args.langfolder_output}/Missings/str_sandbox_missings-{args.langsuffix}.xml'))
-        output_dir.joinpath(f'''language_data.xml''').open('w', encoding='utf-8').writelines(language_data_missings.prettify(formatter='minimal'))
+        language_data_missings.getroot().append(generate_languageFile_element(f'{args.langfolder_output}/Missings/str_missings-{args.langsuffix}.xml'))
+        language_data_missings.getroot().append(generate_languageFile_element(f'{args.langfolder_output}/Missings/str_sandbox_missings-{args.langsuffix}.xml'))
+        write_xml_with_default_setting(language_data_missings, output_dir.joinpath(f'''language_data.xml'''))
         xml_str_missings = generate_string_xml([args.langid])
         for i, r in d.iterrows():
-            new_entry = generate_new_string(r['id'], removeannoyingchars(r['text']))
-            xml_str_missings.base.find('strings').append(new_entry)
+            new_entry = generate_new_string_element(r['id'], removeannoyingchars(r['text']))
+            xml_str_missings.find('strings').append(new_entry)
         print(f'''SUMMARY: {d.shape[0]} entries out of {n_entries_xml} ({100 * (d.shape[0]/n_entries_total):.0f}%) are missing from vanilla {args.langid} language files.''')
-        output_dir.joinpath(f'''str_missings-{args.langsuffix}.xml''').open('w', encoding='utf-8').writelines(xml_str_missings.prettify(formatter='minimal'))
+        write_xml_with_default_setting(xml_str_missings, output_dir.joinpath(f'''str_missings-{args.langsuffix}.xml'''))
         print(f'''saved to {output_dir}''')
 
 
@@ -286,67 +277,80 @@ def output_missings_modulewise(args, output_dir, module, df, df_original=None):
     if d_sub.shape[0] < 1:
         return None
     xml = generate_string_xml([args.langid])
-    strings = xml.base.find('strings', recursive=False)
+    strings = xml.findall('strings')
     for i, r in d_sub.iterrows():
-        strings.append(generate_new_string(r['id'], removeannoyingchars(r['text'])))
-    with output_dir.joinpath(f'translation-missings-{args.langshort}.xml').open('w', encoding='utf-8') as f:
-        f.writelines(xml.prettify(formatter='minimal'))
-    with output_dir.joinpath(f'language_data.xml').open('r', encoding='utf-8') as f:
-        xml_lang_data = BeautifulSoup(f.read(), 'lxml-xml')
+        strings.append(generate_new_string_element(r['id'], removeannoyingchars(r['text'])))
+    write_xml_with_default_setting(xml, output_dir.joinpath(f'translation-missings-{args.langshort}.xml'))
+    with output_dir.joinpath(f'language_data.xml') as fp:
+        xml_lang_data = ET.parse(fp)
     lang_data_xml = xml_lang_data.find('LanguageData')
-    new_entry = generate_languageFile(
+    new_entry = generate_languageFile_element(
         path=f'{args.langshort}/{module}/translation-missings-{args.langfolder_output}.xml'
         )
     lang_data_xml.append(new_entry)
-    with output_dir.joinpath(f'language_data.xml').open('w', encoding='utf-8') as f:
-        f.writelines(lang_data_xml.prettify(formatter='minimal'))
+    lang_data_xml.write(
+        output_dir.joinpath(f'language_data.xml'),
+        pretty_print=True,
+        xml_declaration=True,
+        encoding='utf-8'
+    )
     return d_sub.shape[0]
 
 
-def generate_language_data_xml(module:str, id:str, name:str=None, subtitle:str=None, iso:str=None, dev:str='false') -> BeautifulSoup: 
-    language_data = BeautifulSoup(
+def generate_language_data_xml(module:str, id:str, name:Optional[str]=None, subtitle:Optional[str]=None, iso:Optional[str]=None, dev:str='false')->ET.ElementTree: 
+    language_data = ET.fromstring(
     f'''
     <LanguageData>
     </LanguageData>
-    ''', features='lxml-xml'
+    '''
     )
-    language_data.LanguageData['id'] = id
+    language_data.set('id',id)
     if module == 'Native':
-        language_data.LanguageData['name'] = id if name is None else name
+        language_data.set('name', id if name is None else name)
     if subtitle is not None:
-        language_data.LanguageData['subtitle_extension'] = subtitle
+        language_data.set('subtitle_extension', subtitle)
     if iso is not None:
-        language_data.LanguageData['supported_iso'] = iso
-    language_data.LanguageData['under_development'] = dev
-    return language_data
+        language_data.set('supported_iso', iso)
+    language_data.set('under_development', dev)
+    return ET.ElementTree(language_data)
 
 
-def generate_string_xml(langids:list) -> BeautifulSoup:
-    xml = BeautifulSoup(
+def generate_string_xml(langids:list)->ET.ElementTree:
+    xml = ET.fromstring(
         f'''
         <base>
         <tags></tags>
         <strings></strings>
         </base>
-        ''', features='lxml-xml')
-    [xml.base.tags.append(BeautifulSoup(f'<tags id="{id}" />', features='lxml-xml')) for id in langids]
-    return xml
+        ''')
+    [xml.find('tags').append(ET.fromstring(f'<tag id="{id}" />')) for id in langids]
+    return ET.ElementTree(xml)
 
 
-def generate_tag(langid:str) -> BeautifulSoup:
-    return BeautifulSoup(f'<tag language="{langid}" />', features='lxml-xml')
+def generate_tag_element(langid:str)->ET.ElementTree:
+    return ET.fromstring(f'<tag language="{langid}" />')
 
 
-def generate_languageFile(path:str) -> BeautifulSoup:
-    return BeautifulSoup(f'<LanguageFile xml_path="{path}" />', features='lxml-xml')
+def generate_languageFile_element(path:str)->ET.ElementTree:
+    return ET.fromstring(f'<LanguageFile xml_path="{path}" />')
 
 
-def generate_new_string(id:str, text:str):
-    new_entry = BeautifulSoup(f'''<string id="PLAHECOLHDER" text="[PLACEHOLDER]" />''', features='lxml-xml')
-    new_entry.find('string')['id']= id
-    new_entry.find('string')['text']= text
+def generate_new_string_element(id:str, text:str):
+    new_entry = ET.fromstring(f'''<string id="PLAHECOLHDER" text="[PLACEHOLDER]" />''')
+    new_entry.attrib['id']= id
+    new_entry.attrib['text']= text
     return new_entry
 
+
+def write_xml_with_default_setting(xml:ET.ElementTree, fpath:Path)->bool:
+    ET.indent(xml, space="  ", level=0)
+    xml.write(
+       fpath,
+       pretty_print=True,
+       xml_declaration=True,
+       encoding='utf-8'
+    )
+    return True
 
 if __name__ == '__main__':
     main()
